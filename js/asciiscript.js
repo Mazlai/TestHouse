e2cafdcc4993271998f3b0c5a4e91e18fa9a27c3
Mazlai <micky31200@gmail.com>
2023-11-08 23:53:39 +0100

    frog

--- js/asciiscript.js
+++ js/asciiscript.js
@@ -1,23 +1,48 @@
 const animationElement = document.getElementById('animation');
+const frog = document.getElementById('frog');
 
-        function animate() {
-            const screenWidth = window.innerWidth;
-            const screenHeight = window.innerHeight;
+function animate() {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-            let angle = performance.now() / 1000;
-            let radius = 80 + Math.sin(angle) * 40;
-            let output = '';
+    let angle = performance.now() / 1000;
+    let radius = 80 + Math.sin(angle) * 40;
+    let output = '';
 
-            for (let i = 0; i < 300; i++) {
-                radius += 0.1;
-                const x = Math.cos(angle + i) * radius;
-                const y = Math.sin(angle + i) * radius;
-                output += `<span style="position: absolute; top: ${screenHeight / 2 + y}px; left: ${screenWidth / 2 + x}px;">⣿</span>`;
-            }
+    for (let i = 0; i < 300; i++) {
+        radius += 0.1;
+        const x = Math.cos(angle + i) * radius;
+        const y = Math.sin(angle + i) * radius;
+        output += `<span style="position: absolute; top: ${screenHeight / 2 + y}px; left: ${screenWidth / 2 + x}px;">⣿</span>`;
+    }
 
-            animationElement.innerHTML = output;
+    animationElement.innerHTML = output;
 
-            requestAnimationFrame(animate);
-        }
+    requestAnimationFrame(animate);
+}
 
-        animate();
+function moveFrog() {
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    var frogWidth = frog.offsetWidth;
+    var frogHeight = frog.offsetHeight;
+
+    var maxX = windowWidth - frogWidth;
+    var maxY = windowHeight - frogHeight;
+
+    var randomX = Math.random() * maxX;
+    var randomY = Math.random() * maxY;
+
+    frog.style.left = randomX + 'px';
+    frog.style.top = randomY + 'px';
+
+    setTimeout(moveFrog, 1000); // Déplacer la grenouille toutes les secondes
+}
+
+function startAnimation() {
+    moveFrog();
+    animate();
+}
+
+startAnimation();
