86b0ea6f7c72ab2250e3b97fd270ac3a4e3bf22c
Mazlai <micky31200@gmail.com>
2023-11-11 17:49:07 +0100

    Conservation du thème après rafraichissement

--- js/p4/p4load.js
+++ js/p4/p4load.js
@@ -10,22 +10,45 @@ document.addEventListener('DOMContentLoaded', function() {
   const playButton = document.getElementById('play');
   const rulesButton = document.getElementById('rules');
   const themeBtn = document.getElementById('theme-btn');
+  const themeOptionsButtons = document.querySelectorAll('.theme-option');
   const defaultBtn = document.getElementById('default');
   const firstThemeBtn = document.getElementById('theme1');
   const secondThemeBtn = document.getElementById('theme2');
   const returnBtn = document.getElementById('return-btn');
   const restartButton = document.getElementById('restart');
 
+  // Autres variables (localStorage et P4)
+  let currentTheme = localStorage.getItem('currentTheme');
+  const p4 = new P4('#game');
+
+  // Cache la grille au début
+  gamePage.style.display = 'none';
+
+  // Événement pour le bouton Jouer
+  playButton.addEventListener('click', function() {
+    // Cache la page d'accueil
+    homePage.style.display = 'none';
+
+    // Affiche la grille et le changement de joueur
+    gamePage.style.display = 'block';
+    playerTurn.style.visibility = 'visible';
+
+    // Affiche les pieds de la grille
+    gameElement.classList.add('has-feet');
+
+    // Événements lorsque le bouton de redémarrage est cliqué
+    restartButton.addEventListener('click', function() {
+      gameElement.innerHTML = '';
+      p4.createGrid();
+      restartButton.style.visibility = 'hidden';
+      playerTurn.style.visibility = 'visible';
+    });
+  });
+
   // Événement pour le bouton Thème
   themeBtn.addEventListener('click', function() {
     homePage.style.display = 'none';
     themeOptions.style.display = 'block';
-  });
-
-  // Événement pour le bouton Retour
-  returnBtn.addEventListener('click', function() {
-    homePage.style.display = 'block';
-    themeOptions.style.display = 'none';
   });
 
   // Fonction pour changer de thème
@@ -43,10 +66,20 @@ document.addEventListener('DOMContentLoaded', function() {
     firstThemeBtn.className = theme;
     secondThemeBtn.className = theme;
     returnBtn.className = theme;
+
+    // Enregistre le thème actuel dans le localStorage
+    localStorage.setItem('currentTheme', theme);
+  }
+
+  // Si le thème n'est pas défini dans le localStorage, utilisez le thème par défaut
+  if (!currentTheme) {
+    currentTheme = 'default';
+  } else {
+    // Sinon, changez le thème actuel
+    changeTheme(currentTheme);
   }
 
   // Événements pour les options de thème
-  const themeOptionsButtons = document.querySelectorAll('.theme-option');
   themeOptionsButtons.forEach(function(button) {
     button.addEventListener('click', function() {
       const theme = button.id;
@@ -54,29 +87,9 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-  // Cache la grille au début
-  gamePage.style.display = 'none';
-
-  playButton.addEventListener('click', function() {
-    // Cache la page d'accueil
-    homePage.style.display = 'none';
-
-    // Affiche la grille et le changement de joueur
-    gamePage.style.display = 'block';
-    playerTurn.style.visibility = 'visible';
-
-    // Initialisez le Puissance 4
-    const p4 = new P4('#game');
-
-    // Affiche les pieds de la grille
-    gameElement.classList.add('has-feet');
-
-    // Événements lorsque le bouton de redémarrage est cliqué
-    restartButton.addEventListener('click', function() {
-      gameElement.innerHTML = '';
-      p4.createGrid();
-      restartButton.style.visibility = 'hidden';
-      playerTurn.style.visibility = 'visible';
-    });
+  // Événement pour le bouton Retour
+  returnBtn.addEventListener('click', function() {
+    homePage.style.display = 'block';
+    themeOptions.style.display = 'none';
   });
 });
