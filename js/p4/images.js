9fbd010179dff997b3c3f4b6c34adc67cfebfed5
Mazlai <micky31200@gmail.com>
2023-11-13 16:51:03 +0100

    Ajout des images restantesé

--- js/p4/images.js
+++ js/p4/images.js
@@ -5,6 +5,6 @@ const images = [
     "./images/p4/tokens/simpamarseille.JPG", "./images/p4/tokens/nicokado.PNG", "./images/p4/tokens/cycledevie.png", "./images/p4/tokens/mdm84.png", "./images/p4/tokens/bastoche.jpg",
     "./images/p4/tokens/jmbcaca.jpg", "./images/p4/tokens/lucayamaha.png", "./images/p4/tokens/pasdedroitdauteur.jpg", "./images/p4/tokens/sin.jpg", "./images/p4/tokens/clemcledebras.PNG",
     "./images/p4/tokens/ilestencours.png", "./images/p4/tokens/coutax.jpeg", "./images/p4/tokens/homosapiens.PNG", "./images/p4/tokens/jagercar.JPG", "./images/p4/tokens/hugeface.jpg",
-    "./images/p4/tokens/de-oof.png", "./images/p4/tokens/sqlvoielactee.png", "./images/home/East.jpg", "./images/p4/tokens/psedjango.png", "./images/home/East.jpg",
+    "./images/p4/tokens/de-oof.png", "./images/p4/tokens/sqlvoielactee.png", "./images/p4/tokens/chine.png", "./images/p4/tokens/psedjango.png", "./images/p4/tokens/uh.png",
     "./images/p4/tokens/fapsimulator.png", "./images/p4/tokens/psoenslip.png", "./images/p4/tokens/jp.png", "./images/p4/tokens/abbenoir.jpeg", "./images/p4/tokens/tg.jpg"
 ];
